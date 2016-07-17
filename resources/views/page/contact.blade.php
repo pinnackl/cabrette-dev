1529475fbd63a17d25a3de578ca023a7ee068fb1
antoine-humbert <antoine.humbert@neoxia.com>
2016-07-17 15:39:21 +0200

    Change page contact

--- resources/views/page/contact.blade.php
+++ resources/views/page/contact.blade.php
@@ -5,9 +5,9 @@
     @include('partials.navbar')
 
     <div class="container">
-        <section class="content-section">
+
             <div class="row">
-                <h2 style="background: #fff;border-radius: 3px;box-shadow: 0 3px 7px -3px rgba(0, 0, 0, 0.3);margin-top: 20px">Contact</h2>
+                <h2 class="title-media" style="margin-bottom: 30px !important;">Contact</h2>
                 {!! Form::open(['url' => route('contact'), 'method' => 'POST', 'style' => 'padding-left:20px']) !!}
                     {!! Form::bsText('first_name', null, ['required']) !!}
                     {!! Form::bsText('last_name', null, ['required']) !!}
@@ -16,7 +16,7 @@
                     {!! Form::bsButton('Envoyer') !!}
                 {!! Form::close() !!}
             </div>
-        </section>
+
     </div>
 
 @stop
